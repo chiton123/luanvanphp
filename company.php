c61c03b06a28df1756f09c610f01ce6799155514
chiton123 <batphuongtrinhvoti@gmail.com>
2020-12-31 21:08:00 +0700

    php

---
+++ company.php
@@ -0,0 +1,44 @@
+<?php
+include "connect.php";
+
+$idcompany = $_POST['idcompany'];
+// $idcompany = 1;
+class company{
+	function company($id, $name, $introduction, $address, $idarea, $idowner, $image, $website, $status, $vido, $kinhdo){
+		$this->id = $id;
+		$this->name = $name;
+		$this->introduction = $introduction;
+		$this->address = $address;
+		$this->idarea = $idarea;
+		$this->idowner = $idowner;
+		$this->image = $image;
+		$this->website = $website;
+		$this->status = $status;
+		$this->vido = $vido;
+		$this->kinhdo = $kinhdo;
+	}
+}
+$query = "SELECT * FROM company where c_id = '$idcompany'";
+$data = mysqli_query($conn, $query);
+$mang = array();
+while($row = mysqli_fetch_assoc($data)){
+	array_push($mang, new company(
+		$row['c_id'],
+		$row['c_name'],
+		$row['c_introduction'],
+		$row['c_address'],
+		$row['c_idarea'],
+		$row['c_idowner'],
+		$row['c_image'],
+		$row['c_website'],
+		$row['c_status'],
+		$row['latitude'],
+		$row['longitude']));
+
+}
+echo json_encode($mang);
+
+
+
+
+?>
---
+++ job1.php
@@ -0,0 +1,71 @@
+<?php
+include "connect.php";
+// 0: Viec lam hap dan , 1: thuc tap sinh
+// gender: 0 . k yeu cau, 1: nam , 2: nu
+$kind = $_POST['kind'];
+// $kind = 0;
+class job{
+	function job($id, $name, $idcompany, $img, $area, $idtype, $idprofession, $date, $salary, $idarea, $gender, $experience, $number, $position, $description, $requirement, $benefit, $status,$company_name, $type_job )
+	{
+		$this->id = $id;
+		$this->name = $name;
+		$this->idcompany = $idcompany;
+		$this->img = $img;
+		$this->area = $area;
+		$this->idtype = $idtype;
+		$this->idprofession = $idprofession;
+		$this->date = $date;
+		$this->salary = $salary;
+		$this->idarea = $idarea;
+		$this->gender = $gender;
+		$this->experience = $experience;
+		$this->number = $number;
+		$this->position = $position;
+		$this->description = $description;
+		$this->requirement = $requirement;
+		$this->benefit = $benefit;
+		$this->status = $status;
+		$this->company_name = $company_name;
+		$this->type_job = $type_job;
+	}
+
+}
+$mang = array();
+if($kind == 3){
+	$query = "SELECT * FROM job j, company c, area a, typeofwork t where j.j_idcompany = c.c_id and a.ar_id = j.j_idarea and t.t_id = j.j_idtype and j_idtype = 3";
+}else {
+	$query = "SELECT * FROM job j, company c, area a, typeofwork t where j.j_idcompany = c.c_id and a.ar_id = j.j_idarea and t.t_id = j.j_idtype";
+}
+
+$data = mysqli_query($conn, $query);
+while($row = mysqli_fetch_assoc($data)){
+	array_push($mang, new job(
+		$row['j_id'],
+		$row['j_name'],
+		$row['j_idcompany'],
+		$row['c_image'],
+		$row['ar_name'],
+		$row['j_idtype'],
+		$row['j_idprofession'],
+		$row['j_submission_date'],
+		$row['j_salary'],
+		$row['j_idarea'],
+		$row['j_gender'],
+		$row['j_experience'],
+		$row['j_number'],
+		$row['j_position'],
+		$row['j_description'],
+		$row['j_requirement'],
+		$row['j_benefit'],
+		$row['j_status'],
+		$row['c_name'],
+		$row['t_name']
+	));
+
+}
+echo json_encode($mang);
+
+
+
+
+?>

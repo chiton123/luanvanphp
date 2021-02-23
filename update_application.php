d2fd00a4ad382622a936afef92404c64fc37af74
chiton123 <batphuongtrinhvoti@gmail.com>
2021-02-23 16:36:38 +0700

    3

---
+++ update_application.php
@@ -0,0 +1,17 @@
+<?php
+include "connect.php";
+
+$ap_id = $_POST['ap_id'];
+$status = $_POST['status'];
+$timestamp = date('Y-m-d H:i:S', time());
+$query = "UPDATE application set ap_status = '$status' where ap_id = '$ap_id'";
+$result = mysqli_query($conn, $query);
+if($result){
+	echo "success";
+}else {
+	echo "fail";
+}
+
+
+
+?>

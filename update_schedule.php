a0a2f834aa3c4f0699203bcc9d6e0caf298e717b
chiton123 <batphuongtrinhvoti@gmail.com>
2021-03-07 17:27:36 +0700

    3

---
+++ update_schedule.php
@@ -0,0 +1,25 @@
+<?php
+include "connect.php";
+
+$id_sche = $_POST['id_sche'];
+$idjob = $_POST['idjob'];
+$iduser = $_POST['iduser'];
+$type_schedule = $_POST['type_schedule'];
+$date = $_POST['date'];
+$start = $_POST['start'];
+$end = $_POST['end'];
+$note = $_POST['note'];;
+
+$timestamp = date('Y-m-d H:i:S', time());
+$query = "UPDATE schedule SET sc_idjob = '$idjob', sc_iduser = '$iduser', sc_type = '$type_schedule',
+sc_date = '$date', sc_start = '$start' , sc_end = '$end', sc_note = '$note' WHERE sc_id = '$id_sche'";
+$result = mysqli_query($conn, $query);
+if($result){
+	echo "success";
+
+}else{
+	echo "fail";
+}
+
+
+?>

dae5f2a9d181464c043386ea787b6277f75c2676
chiton123 <batphuongtrinhvoti@gmail.com>
2021-03-16 16:32:29 +0700

    3

--- get_notification_recruiter.php
+++ get_notification_recruiter.php
@@ -1,10 +1,9 @@
 <?php
 include "connect.php";
-
 $id_recruiter = $_POST['id_recruiter'];
 // $id_recruiter = 3;
 class notification{
-	function notification($id, $ap_id, $job_id, $type_notification, $type_user, $id_user, $content,$status, $img, $date_read){
+	function notification($id, $ap_id, $job_id, $type_notification, $type_user, $id_user, $content,$status, $img, $date_read, $ap_status, $ap_note){
 		$this->id = $id;
 		$this->ap_id = $ap_id;
 		$this->job_id = $job_id;
@@ -15,6 +14,8 @@ class notification{
 		$this->status = $status;
 		$this->img = $img;
 		$this->date_read = $date_read;
+		$this->ap_status = $ap_status;
+		$this->ap_note = $ap_note;
 	}
 }
 $query = "SELECT * FROM application a, notification n, job j, company c where a.ap_id = n.n_id_application and n.n_iduser = '$id_recruiter' and j.j_id = a.ap_jobid and c.c_id = j.j_idcompany and j.j_status_delete = 0 and j.j_status_post = 0 order by n.n_id desc";
@@ -31,7 +32,9 @@ while($row = mysqli_fetch_assoc($result)){
 		$row['n_content'],
 		$row['n_status'],
 		$row['c_image'],
-		$row['n_read_at']
+		$row['n_read_at'],
+		$row['ap_status'],
+		$row['ap_note']
 	));
 }
 echo json_encode($mang);
